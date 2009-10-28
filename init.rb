95332ecb2e39fd93b3265d96df683b2e4e210869
Sebastian Roebke <sebastian.roebke@xing.com>
2009-10-28 14:59:08 +0100

    Removed unnecessary (even disabled) code fragment

--- init.rb
+++ init.rb
@@ -1,7 +1,5 @@
 require "field_error_proc_helper"
 
-#include FieldErrorProcHelper
-
 ActionView::Base.field_error_proc = Proc.new do |html_element, instance|
   FieldErrorProcHelper.add_css_class_to_element('input-error', html_element)
 end
